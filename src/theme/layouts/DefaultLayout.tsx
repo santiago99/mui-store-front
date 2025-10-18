48f9bbd476df3b81e19fa54b18ef02287fa092a1
Pavel Zobnin <mbair@192.168.1.55>
2025-10-18 14:41:04 -0300

    FEAT: Navigation updtate — breadcrumb in default layout

--- src/theme/layouts/DefaultLayout.tsx
+++ src/theme/layouts/DefaultLayout.tsx
@@ -1,13 +1,14 @@
 import * as React from "react";
 import { Outlet } from "react-router-dom";
 import CssBaseline from "@mui/material/CssBaseline";
-import type {} from "@mui/material/themeCssVarsAugmentation";
+import type { } from "@mui/material/themeCssVarsAugmentation";
 import AppTheme from "@/theme/AppTheme";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 
 import Sidebar from "@/theme/components/Sidebar";
 import Navbar from "@/theme/components/Navbar";
+import Breadcrumb from "@/theme/components/Breadcrumb";
 import { useRouteChange } from "@/features/navigation/useRouteChange";
 
 // const StyledBreadcrumbs = styled(Breadcrumbs)(({ theme }) => ({
@@ -61,6 +62,7 @@ export default function DefaultLayout() {
               px: 2,
             }}
           >
+            <Breadcrumb />
             <Outlet />
           </Box>
         </Box>
