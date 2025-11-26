2c024f811914c977cfa17dadc651f72f86a267b6
Pavel Zobnin <paul.zobnin@gmail.com>
2025-11-26 14:15:40 -0300

    REFACTOR: mote strict type def to prevent linting error

--- src/features/category/categoryPreloader.ts
+++ src/features/category/categoryPreloader.ts
@@ -1,10 +1,12 @@
 import { store } from "@/app/store";
 import { apiSlice } from "@/app/apiSlice";
 
-export async function categoryPreloader(args: any) {
+export async function categoryPreloader(args: {
+  params: { categoryId: string };
+}) {
   const { params } = args;
   const categoryId = parseInt(params.categoryId, 10);
-  
+
   if (isNaN(categoryId)) {
     throw new Response("Category ID must be a number", { status: 400 });
   }
