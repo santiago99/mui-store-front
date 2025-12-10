283fd67055c7f152c58169f03cf41e19f1982341
Pavel Zobnin <paul.zobnin@gmail.com>
2025-12-10 15:42:10 -0300

    CHORE: visualisation plugin for buld package size. React extacted into separate chunk in build package

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,44 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react-swc";
+import swc from "@vitejs/plugin-react-swc";
 import path from "path";
+//import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [swc(), tailwindcss() /* , visualizer({ open: true }) */],
   resolve: {
     alias: [{ find: "@", replacement: path.resolve("./src") }],
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id: string) {
+          // React
+          if (
+            id.includes("node_modules/react/") ||
+            id.includes("node_modules/react-dom/")
+          ) {
+            return "react";
+          }
+          // Put big libraries in a separate chunk
+          /* if (
+            id.includes("node_modules/@radix-ui/") ||
+            id.includes("node_modules/@floating-ui/") ||
+            id.includes("node_modules/@reduxjs/toolkit/")
+          ) {
+            return "vendor";
+          } */
+        },
+        /* [
+            "i18next",
+            "i18next-browser-languagedetector",
+            //"@radix-ui",
+            //"@floating-ui",
+            "@reduxjs/toolkit",
+          ], */
+      },
+    },
   },
   /* server: {
     open: true,
